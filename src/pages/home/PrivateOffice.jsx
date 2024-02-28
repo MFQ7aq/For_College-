09fef9bebd153264c5709a66a1cafeb67043063f
Ilya <ilyazhukov128@gmail.com>
2024-02-28 11:15:02 +0600

    28.02 push

--- src/pages/home/PrivateOffice.jsx
+++ src/pages/home/PrivateOffice.jsx
@@ -1,12 +1,21 @@
+import { useCallback } from "react";
 import NavBar from "../../components/NavBar"
-import { Link, useNavigate  } from "react-router-dom"
+import { Link } from "react-router-dom"
+import { useNavigate } from "react-router-dom";
 
 function PrivateOffice() {
   const navigate = useNavigate();
 
+  const handleLogout = useCallback(() => {
+    localStorage.removeItem('token');
+  }, []);
+
   const Back = () => {
     navigate(-1);
   };
+  const reolad = () => {
+    window.location.reload()
+  }
 
   return (
     <div className="private-office-contents">
@@ -19,10 +28,9 @@ function PrivateOffice() {
           <div className="avatar__container"><div className="avatar"></div></div>
           <h4 className="user__name">Test</h4>
           <ul className="config__list">
-            <li className="config__items-li"><Link to="" className="config__items">Редактировать профиль</Link></li>
+          <li className="config__items-li"><Link to="/Personal_data" className="config__items">Редактировать профиль</Link></li>
             <li className="config__items-li"><Link to="" className="config__items">Моя учётная запись</Link></li>
-            <li className="config__items-li"><Link to="" className="config__items">Выйти</Link></li>
-            <li className="config__items-li"><Link to="" className="config__items">Отмена</Link></li>
+            <li className="config__items-li"><Link to="/Authorization"  onClick={handleLogout} className="config__items">Выйти</Link></li>
           </ul>
         </div>
           <div className="office">
@@ -49,7 +57,7 @@ function PrivateOffice() {
                 <input type="text" className="input__office"/></label>
             </div>
             <div className="btn__office">
-              <Link to="/Personal_data" className="btn__link btn__green montherat">Данные</Link>
+              <button onClick={reolad} className="btn__link btn__green montherat">Данные</button>
               <button onClick={Back} className="btn__link btn__blue montherat">Назад</button>
             </div>
           </div>
