f77c81fa4663cd0dfe605086f56ec607f711bf23
Ilya <ilyazhukov128@gmail.com>
2024-02-29 08:34:53 +0600

    29.02 Отправка на info/add и вроде как отправка на progress/add

--- src/pages/home/PersonalData.jsx
+++ src/pages/home/PersonalData.jsx
@@ -1,37 +1,18 @@
 import { useState, useCallback, useEffect } from "react";
 import axios from "axios";
 import NavBar from "../../components/NavBar";
-import { useNavigate } from "react-router-dom";
-import MultipleSelectCheckmarks from "../../components/CheckMark";
 
 function UserInfo() {
-  const navigate = useNavigate();
-  let token = localStorage.getItem('token')
-
-  const [institutes, setInstitutes] = useState([]);
-  const [positions, setPositions] = useState([]);
   const [degree, setDegree] = useState([]);
   const [rank, setRank] = useState([]);
   const [stateAwards, setStateAwards] = useState([]);
-  const [name, setName] = useState('');
-  const [password, setPassword] = useState('');
   const [selectedValues, setSelectedValues] = useState({
-    inst: '',
-    post: '',
-    stat: '',
     degree: '',
     rank: '',
     awards: [],
     links: {}
   });
   const [customInputValue, setCustomInputValue] = useState('');
-
-  const handleChangeLinks = useCallback(links => {
-    setSelectedValues(prevValues => ({
-      ...prevValues,
-      links: links
-    }));
-  }, []);
 
   const handleSelect = useCallback((field, value) => {
     setSelectedValues(prevValues => ({
@@ -42,49 +23,53 @@ function UserInfo() {
 
   const fetchData = useCallback(async () => {
     try {
-      const response = await axios.get('http://localhost:8092/api/user/fill',
-      {
-        headers: { Authorization: `Bearer ${token}` }
-      });
-      const { institutes, positions, degree, rank, state_awards } = response.data;
-      setInstitutes(institutes);
-      setPositions(positions);
+      const response = await axios.get('http://localhost:8092/api/user/fill');
+      const { degree, rank, state_awards } = response.data;
       setDegree(degree);
       setRank(rank);
       setStateAwards(state_awards);
     } catch (error) {
       console.log(error);
     }
-  }, [token]);
-
+  }, [setDegree, setRank, setStateAwards]);
+  
   useEffect(() => {
     fetchData();
   }, [fetchData]);
 
-  const handleSubmit = useCallback((e) => {
+  const handleSubmit = useCallback(async (e) => {
     e.preventDefault();
-    const post = selectedValues.post === "Другое" ? customInputValue : selectedValues.post;
-    axios.post('http://localhost:8092/pps/sign-up', {
-      "username": name,
-      "password": password,
-      "post": post,
-      "inst": selectedValues.inst,
-      "stat": selectedValues.stat,
-      "degree": selectedValues.degree,
-      "rank": selectedValues.rank,
-      "awards": selectedValues.awards,
-      "links": selectedValues.links,
-    })
-      .then(function (response) {
-        if (response.status >= 200 && response.status <= 204) {
-          navigate(-1);
-          console.log(response);
+    const formData = {
+      degree: selectedValues.degree,
+      rank: selectedValues.rank,
+      awards: {
+        "a": { id: 1, link: "" },
+        "b": { id: 2, link: "" },
+        "c": { id: 3, link: "" },
+        "d": { id: 4, link: "" },
+      },
+    };
+    stateAwards.forEach((award) => {
+      const checkbox = document.querySelector(`input[name="${award.name}"]:checked`);
+      const linkInput = document.querySelector(`input[name="${award.name}_link"]`);
+      if (checkbox) {
+        formData.awards[award.name] = {
+          id: award.id,
+          link: linkInput.value
+        };
+      }
+    });
+    try {
+      const token = localStorage.getItem('token');
+      await axios.post('http://localhost:8092/api/user/progress/add', formData, {
+        headers: {
+          Authorization: `Bearer ${token}`
         }
-      })
-      .catch(function (error) {
-        console.log(error);
       });
-  }, [customInputValue, name, navigate, password, selectedValues, token]);
+    } catch (error) {
+      console.error(error);
+    }
+  }, [selectedValues, stateAwards]);
 
   return (
     <div className="сontents">
@@ -96,30 +81,9 @@ function UserInfo() {
         <div className="auth__contain-doble">
           <label htmlFor="" className="auth__label">
             <form onSubmit={handleSubmit} className="auth_auth">
-              <input type="text" className="auth__input Montherat" value={name} onChange={e => setName(e.target.value)} placeholder="ФИО (Полностью)" />
-              <select value={selectedValues.inst} onChange={(e) => handleSelect('inst', e.target.value)} className="auth__input auth__select Montherat">
-                <option value="">Институт</option>
-                {institutes.map((inst) =>
-                  <option key={inst.id} value={inst.name}>
-                    {inst.name}
-                  </option>)}
-              </select>
-              <select value={selectedValues.post} onChange={(e) => handleSelect('post', e.target.value)} className="auth__input auth__select Montherat">
-                <option value="">Должность</option>
-                {positions.map((post) =>
-                  <option key={post.id} value={post.name}>
-                    {post.name}
-                  </option>)}
-                <option value="Другое">Другое</option>
-              </select>
               {selectedValues.post === "Другое" && (
                 <input type="text" className="auth__input Montherat" value={customInputValue} onChange={(e) => setCustomInputValue(e.target.value)} placeholder="Введите другую должность" />
               )}
-              <select value={selectedValues.stat} onChange={(e) => handleSelect('stat', e.target.value)} className="auth__input auth__select Montherat">
-                <option value="">штат/совм.</option>
-                <option value="Штат">Штат</option>
-                <option value="Совместитель">Совместитель</option>
-              </select>
               <select value={selectedValues.degree} onChange={(e) => handleSelect('degree', e.target.value)} className="auth__input auth__select Montherat">
                 <option value="">Ученая степень</option>
                 {degree.map((degree) =>
@@ -134,13 +98,16 @@ function UserInfo() {
                     {rank.name}
                   </option>)}
               </select>
-              <MultipleSelectCheckmarks
-                data={stateAwards}
-                value={selectedValues.awards}
-                onChange={(value) => handleSelect('awards', value)}
-                links={selectedValues.links} onChangeLinks={handleChangeLinks}
-              />
-              <input type="email" className="auth__input Montherat" value={password} onChange={e => setPassword(e.target.value)} placeholder="Email" />
+              <div className="awards">
+              {stateAwards.map((awards) =>
+                <div className="awards__block" key={awards.id} value={awards.name}>
+                  <div className="auth__input awards__select Montherat">
+                    <input type="checkbox" className="checkbox" name={awards.name} id="" />
+                    {awards.name}
+                  </div>
+                  <input type="text" className="auth__input awards__select Montherat" name={`${awards.name}_link`} placeholder="Введите ссылку" />
+                </div>)}
+              </div>
             </form>
           </label>
           <div className="auth__btn-center">
