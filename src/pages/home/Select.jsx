442d61a2de5defc68e8bb53bdbc7bb0a7252d7b5
Ilya <ilyazhukov128@gmail.com>
2024-03-19 11:40:51 +0600

    19.03 Всё есть, нечего не работает

---
+++ src/pages/home/Select.jsx
@@ -0,0 +1,79 @@
+import * as React from 'react';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import ListItemText from '@mui/material/ListItemText';
+import Select from '@mui/material/Select';
+import Checkbox from '@mui/material/Checkbox';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+
+export default function MultipleSelectCheckmarks({ data }) {
+  const [selectedOptions, setSelectedOptions] = React.useState([]);
+  const [linkInputs, setLinkInputs] = React.useState({});
+
+  const handleOptionChange = (event) => {
+    setSelectedOptions(event.target.value);
+  };
+
+  const handleLinkChange = (id) => (event) => {
+    setLinkInputs({ ...linkInputs, [id]: event.target.value });
+  };
+
+  const handleAddLinkClick = (id) => {
+    setLinkInputs({ ...linkInputs, [id]: '' });
+  };
+
+  const handleLinkClick = (event) => {
+    event.stopPropagation(); // Предотвращаем всплытие события клика
+  };
+
+  return (
+    <div>
+      <FormControl sx={{ m: 1, width: 300 }}>
+        <InputLabel id="demo-multiple-checkbox-label">{data.name}</InputLabel>
+        <Select
+          labelId="demo-multiple-checkbox-label"
+          id="demo-multiple-checkbox"
+          multiple
+          value={selectedOptions}
+          onChange={handleOptionChange}
+          input={<OutlinedInput label={data.name} />}
+          renderValue={(selected) => selected.join(', ')}
+        >
+          {data.researchActivitiesSubtitles.map((item) => (
+            <MenuItem key={item.id} value={item.name}>
+              <Checkbox checked={selectedOptions.indexOf(item.name) > -1} />
+              <ListItemText primary={item.name} />
+              {selectedOptions.includes(item.name) && (
+                <div>
+                  <TextField
+                    id={`link-input-${item.id}`}
+                    label={`Link for ${item.name}`}
+                    value={linkInputs[`link-input-${item.id}`] || ''}
+                    onChange={handleLinkChange(`link-input-${item.id}`)}
+                    onClick={handleLinkClick}
+                    margin="normal"
+                    variant="outlined"
+                  />
+                  <Button
+                    onClick={(e) => {
+                      handleAddLinkClick(`link-input-${item.id}`);
+                      e.stopPropagation();
+                    }}
+                    variant="outlined"
+                    size="small"
+                    sx={{ marginLeft: '10px' }}
+                  >
+                    Добавить
+                  </Button>
+                </div>
+              )}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    </div>
+  );
+}
--- src/pages/home/Ural.jsx
+++ src/pages/home/Ural.jsx
@@ -1,109 +1,91 @@
 import { useState, useCallback, useEffect } from "react";
 import axios from "axios";
 import NavBar from "../../components/NavBar";
-import RegNav from "../../components/RegNav"
+import RegNav from "../../components/RegNav";
 import AccountConf from "../../components/AccountConf";
 import { useNavigate } from "react-router-dom";
+import Select from "./Select";
 
 function Ural() {
-  const token = localStorage.getItem("token")
+  const token = localStorage.getItem("token");
   const navigate = useNavigate();
-  const [publications, setPublications] = useState([]);
-  const [hIndex, setHIndex] = useState([]);
-  const [patent, setPatent] = useState([]);
-  const [dissertationDefense, setDissertationDefense] = useState([]);
-  const [selectedValues, setSelectedValues] = useState({
-    publications: "",
-    hIndex: "",
-    patent: "",
-    dissertationDefense: "",
-  });
-
-  const handleSelect = useCallback((field, value) => {
-    setSelectedValues(prevValues => ({
-      ...prevValues,
-      [field]: value
-    }));
-  }, []);
+  const [data, setData] = useState([]);
+  const [selectedValues, setSelectedValues] = useState({});
 
   const fetchData = useCallback(async () => {
     try {
-      const response = await axios.get('http://localhost:8092/api/user/progress');
-      const data = response.data[0];
-      const publications = data.find(item => item.name === 'Публикации в научном журнале «Наука и инновационные технологии» МУИТ');
-      const hIndex = data.find(item => item.name === 'Индекс Хирша по публикациям');
-      const patent = data.find(item => item.name === 'Патент / Авторское свидетельство');
-      const dissertationDefense = data.find(item => item.name === 'Защита диссертации');
-
-      setPublications(publications ? publications.value : []);
-      setHIndex(hIndex ? hIndex.value : []);
-      setPatent(patent ? patent.value : []);
-      setDissertationDefense(dissertationDefense ? dissertationDefense.value : []);
-      console.log(hIndex);
+      const response = await axios.get("http://localhost:8092/api/user/research");
+      const userData = response.data[0];
+      setData(userData);
+      const initialSelectedValues = {};
+      userData.forEach((research) => {
+        initialSelectedValues[research.id] = "";
+      });
+      setSelectedValues(initialSelectedValues);
     } catch (error) {
       console.log(error);
     }
-  }, [setPublications, setHIndex, setPatent, setDissertationDefense]);
+  }, []);
+
   useEffect(() => {
     fetchData();
   }, [fetchData, token]);
+
+  // const handleSelect = useCallback((field, value) => {
+  //   setSelectedValues((prevValues) => ({
+  //     ...prevValues,
+  //     [field]: { subId: value, link: "" },
+  //   }));
+  // }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const payload = {
+        ural: selectedValues,
+      };
+      const response = await axios.post("http://localhost:8092/api/user/research/add", payload, {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   const Back = useCallback(() => {
     navigate(-1);
   }, [navigate]);
 
   return (
-  <div className="private-office-contents">
-    <div className="header">
-      <NavBar/>
-      <div className="private-office-bg">
-      <RegNav/>
+    <div className="private-office-contents">
+      <div className="header">
+        <NavBar />
+        <div className="private-office-bg">
+          <RegNav />
+        </div>
       </div>
-    </div>
-    <div className="private-office__main">
-    <AccountConf />
-      <div className="auth__contain-doble">
-        <label htmlFor="" className="auth__label">
-          <form>
-            <div className="auth_auth">
-              <select value={selectedValues.publications} onChange={(e) => handleSelect('publications', e.target.value)} className="input__office input__text-s Montherat">
-                <option value="">«Наука и инновационные технологии» МУИТ</option>
-                {publications.map((publications) =>
-                  <option key={publications.id} value={publications.id}>
-                    {publications.name}
-                  </option>
-                )}
-              </select>
-              <select value={selectedValues.hIndex} onChange={(e) => handleSelect('hIndex', e.target.value)} className="input__office input__text-s Montherat">
-                <option value="">Индекс Хирша по публикациям</option>
-                {hIndex.map((hIndex) =>
-                  <option key={hIndex.id} value={hIndex.id}>
-                    {hIndex.name}
-                  </option>
-                )}
-              </select>
-              <select value={selectedValues.patent} onChange={(e) => handleSelect('patent', e.target.value)} className="input__office input__text-s Montherat">
-                <option value="">Патент / Авторское свидетельство</option>
-                {patent.map((patent) =>
-                  <option key={patent.id} value={patent.id}>
-                    {patent.name}
-                  </option>
-                )}
-              </select>
-              <select value={selectedValues.dissertationDefense} onChange={(e) => handleSelect('dissertationDefense', e.target.value)} className="input__office input__text-s Montherat">
-                <option value="">Защита диссертации</option>
-                {dissertationDefense.map((dissertationDefense) =>
-                  <option key={dissertationDefense.id} value={dissertationDefense.id}>
-                    {dissertationDefense.name}
-                  </option>
-                )}
-              </select>
-            </div>
-            {/* <button className="bnt__reg btn__green btn__link" onClick={handleSubmit}>Отправить</button> */}
-            <button onClick={Back} className="btn__link btn__blue montherat">Назад</button>
-          </form>
-        </label>
-      </div>
+      <div className="private-office__main">
+        <AccountConf />
+        <div className="auth__contain-doble">
+          <label htmlFor="" className="auth__label">
+            <form>
+              <div className="auth_auth-c">
+              {data.map((research) => (
+                <Select data={research} key={research.id}></Select>
+              ))}
+              </div>
+              <button className="bnt__reg btn__green btn__link" onClick={handleSubmit}>
+                Отправить
+              </button>
+              <button onClick={Back} className="btn__link btn__blue montherat">
+                Назад
+              </button>
+            </form>
+          </label>
+        </div>
       </div>
     </div>
   );
