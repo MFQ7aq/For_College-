0f48300993157f3e052b0597b8a64d84b516307c
Ilya <ilyazhukov128@gmail.com>
2024-05-22 11:38:41 +0600

    22.05

--- src/pages/home/Redact.jsx
+++ src/pages/home/Redact.jsx
@@ -33,7 +33,7 @@ const Redact = () => {
       const bag = {};
       userData.userAwards.concat(userData.userResearch, userData.userInnovative, userData.userSocial).forEach(item => {
         const editedLink = editedLinks[item.id] || "";
-        const linkToSave = editedLink.trim() === "" ? "" : editedLink;
+        const linkToSave = editedLink.trim() === "" ? "Нет ссылки" : editedLink;
         bag[item.id] = {
           id: item.id,
           link: linkToSave,
