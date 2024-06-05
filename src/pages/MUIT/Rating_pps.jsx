2236928cde574637425fb453814b674789151189
Ilya <ilyazhukov128@gmail.com>
2024-06-05 11:17:54 +0600

    04.06

--- src/pages/MUIT/Rating_pps.jsx
+++ src/pages/MUIT/Rating_pps.jsx
@@ -32,23 +32,18 @@ function Rating_pps() {
   return (
     <div className="сontents">
       <div className="header">
-        <NavBar/>
+        <NavBar />
       </div>
       <div className="main">
         <div className="title__table-un">
           <h2 className="Edu__text-L">Рейтинг ППС</h2>
           <label htmlFor="" className="search__label">
-            <input type="text" className="search__input-rating"/>
+            <input type="text" className="search__input-rating" />
             <div className="search__btn-rating"><div className="search__btn-in"></div>
-          </div>
-        </label>
+            </div>
+          </label>
         </div>
         <div className="sort-buttons">
-          <button onClick={() => sortData('sum')}>Сортировать по Итоговой сумме</button>
-          <button onClick={() => sortData('awardPoints')}>Сортировать по Личным достижениям</button>
-          <button onClick={() => sortData('researchPoints')}>Сортировать по Научно-исследовательской деятельности</button>
-          <button onClick={() => sortData('innovativePoints')}>Сортировать по Инновационно-образовательной деятельности</button>
-          <button onClick={() => sortData('socialPoints')}>Сортировать по Воспитательной, общественной деятельности</button>
         </div>
         <table className="table">
           <thead>
@@ -56,29 +51,29 @@ function Rating_pps() {
               <th>№</th>
               <th>ФИО</th>
               <th>Институты</th>
-              <th>I.Личные достижения</th>
-              <th>II. Научно-исследовательская деятельность</th>
-              <th>III. Инновационно-образовательная деятельность</th>
-              <th>IV. Воспитательная, общественная деятельность</th>
-              <th>Итого</th>
+              <th onClick={() => sortData('awardPoints')}>I.Личные достижения</th>
+              <th onClick={() => sortData('researchPoints')}>II. Научно-исследовательская деятельность</th>
+              <th onClick={() => sortData('innovativePoints')}>III. Инновационно-образовательная деятельность</th>
+              <th onClick={() => sortData('socialPoints')}>IV. Воспитательная, общественная деятельность</th>
+              <th onClick={() => sortData('sum')}>Итого</th>
             </tr>
           </thead>
           <tbody>
-          {userData.map((data) => (
-            <tr key={data.id}>
-              <td>{data.id}</td>
-              <td><Link to={`http://localhost:5173/user/${data.id}`}>{data.name}</Link></td>
-              <td>{data.institute}</td>
-              <td>{data.awardPoints}</td>
-              <td>{data.researchPoints}</td>
-              <td>{data.innovativePoints}</td>
-              <td>{data.socialPoints}</td>
-              <td>{data.sum}</td>
-            </tr>
-          ))}
+            {userData.map((data) => (
+              <tr key={data.id}>
+                <td>{data.id}</td>
+                <td><Link to={`http://localhost:5173/user/${data.id}`}>{data.name}</Link></td>
+                <td>{data.institute}</td>
+                <td>{data.awardPoints}</td>
+                <td>{data.researchPoints}</td>
+                <td>{data.innovativePoints}</td>
+                <td>{data.socialPoints}</td>
+                <td>{data.sum}</td>
+              </tr>
+            ))}
           </tbody>
         </table>
-        <BackButton/>
+        <BackButton />
       </div>
     </div>
   )
