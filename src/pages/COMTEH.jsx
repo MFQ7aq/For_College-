0535cd883b852a1d4139862a5494e2219294f095
Ilya <ilyazhukov128@gmail.com>
2024-06-10 12:48:32 +0600

    09.06

--- src/pages/COMTEH.jsx
+++ src/pages/COMTEH.jsx
@@ -1,49 +1,67 @@
 import NavBar from "../components/NavBar"
 import BackButton from "../components/Back"
-import { Link } from "react-router-dom"
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function Comteh() {
+  const [inst, setInst] = useState([]);
+  const [sortedField, setSortedField] = useState('sum');
+  const [filteredInst, setFilteredInst] = useState([]);
+
+  useEffect(() => {
+    const getInst = async () => {
+      try {
+        const response = await axios.get('http://localhost:8092/api/rating/comtehno/departments');
+        const data = response.data.institutions;
+        setInst(data);
+        setFilteredInst(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getInst();
+  }, []);
+
+  useEffect(() => {
+    sortData(sortedField);
+  }, [sortedField]);
+
+  const sortData = (field) => {
+    const sortedData = [...filteredInst].sort((a, b) => b[field] - a[field]);
+    setFilteredInst(sortedData);
+    setSortedField(field);
+  };
+
   return (
     <div className="сontents">
       <div className="header">
-        <NavBar/>
+        <NavBar />
       </div>
       <div className="main">
-        <button className="btn" ><Link to="/" className="btn__link" >Инструкция по заполнению анкеты</Link></button>
         <div className="Edu__logo-name">
           <div className="Komteh__logo"></div>
           <div className="Edu__text-M Komteh__name">Бишкекский колледж компьютерных систем и технологий</div>
         </div>
-        <div className="Edu__table">
-          <div className="row">
-            <h5>Отделения</h5>
-          </div>
-          <div className="row">
-            <h5>Средний балл</h5>
-          </div>
-          <div className="row">
-            <h5>Итого</h5>
-          </div>
-          <div className="row">
-          <Link to="/COMTEHNO/IVTD" className="h6">Информатика, вычислительная техника и дизайна</Link>
-          </div>
-          <div className="row">
-            <h6 className="h6">865</h6>
-          </div>
-          <div className="row">
-            <h6 className="h6">865</h6>
-          </div>
-          <div className="row">
-            <Link to="/COMTEHNO/EUBD" className="h6">Экономика, управление и банковское дело</Link>
-          </div>
-          <div className="row">
-            <h6 className="h6">504</h6>
-          </div>
-          <div className="row">
-            <h6 className="h6">504</h6>
-          </div>
-        </div>
-        <BackButton/>
+        <table className="table">
+          <thead>
+            <tr>
+              <th className="un_l">Бишкекский колледж компьютерных систем и технологий</th>
+              <th className="sorter un_l" onClick={() => sortData('middlePoints')}>Средний балл ППС</th>
+              <th className="sorter un_l" onClick={() => sortData('sum')}>Итого</th>
+            </tr>
+          </thead>
+          <tbody>
+            {filteredInst.map((institution, i) => (
+              <tr key={i}>
+                <td>{institution.name}</td>
+                <td>{institution.middlePoints}</td>
+                <td>{institution.sum}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <BackButton />
       </div>
     </div>
   )
